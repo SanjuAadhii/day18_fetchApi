e397bfde082584a9ac9e6cde5c9c8e2eb6448f3e
sanjayaadhith <sanjayaadhith007@gmail.com>
2024-03-02 02:02:20 +0530

    Upaded Api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var res = fetch("https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json").then(data => data.json()).then(data1 => bar(data1))
+var res = fetch("https://restcountries.com/v3.1/all").then(data => data.json()).then(data1 => bar(data1))
 console.log(res)
 // Api key = 14a494807d3a665e0f8c636b1b82f7b2 
 
@@ -21,14 +21,14 @@ row.style.marginTop="60px"
 function bar(data){
     console.log(data)
     for(let i=0;i<data.length;i++){
-        console.log(data[i].flag)
+        
         var col = document.createElement("div");
         col.className="col-lg-4 col-sm-12";
         col.style.marginBottom = "30px";
         col.innerHTML=`<div class="card text-center " style="width: 18rem;background: rgb(131,58,180);
         background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,0.3981967787114846) 50%, rgba(252,176,69,1) 100%);">
-        <h5 class="card-title" style="background-color:black;color:white;padding:2px" >${data[i].name}</h5>
-        <img src="${data[i].flag}" class="card-img-top" alt="flag">
+        <h5 class="card-title" style="background-color:black;color:white;padding:5px" >${data[i].name.common}</h5>
+        <img src="${data[i].flags.svg}" style="padding:10px; height:200px" alt="flag">
         <div class="card-body ">
         <p class="card-text">Capital : ${data[i].capital}</p>
         <p class="card-text">Region : ${data[i].region}</p>
